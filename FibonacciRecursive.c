764e69627ddc995fee95978991d9df085f668196
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-11-17 18:25:43 +0300

    FibonacciRecursive has been added.

---
+++ FibonacciRecursive.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int recursiveFibonacci(int n){
+    if(n<= 1){
+        return n;
+    }else{
+        return recursiveFibonacci(n-1) + recursiveFibonacci(n-2);
+    }
+}
+
+int main(){
+    int n;
+
+    printf("Enter the number of terms that you want the see in the Fibonacci sequence:");
+    scanf("%d",&n);
+
+    printf("Fibonaci series for %d elements. \n",n);
+    for(int i=0; i<n; i++){
+        printf("%d ", recursiveFibonacci(i));
+    }
+}
